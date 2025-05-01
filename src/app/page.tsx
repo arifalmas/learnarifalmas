395c9c3054c07f3b2fcd05fb6da385adfa899f29
arifalmas <freelancerarifar@gmail.com>
2025-05-01 16:42:27 +0600

    ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="saira grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-[32px] sm:text-[48px] font-bold text-center sm:text-left">
-          Hello, Welcome to Learn with Arif Almas
-        </h1>
         <Image
           className="dark:invert center justify-center"
           src="/learnarifalmas.png"
@@ -15,6 +12,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1 className="text-[32px] sm:text-[48px] font-bold text-center sm:text-left">
+          Hello, Welcome to Learn with Arif Almas
+        </h1>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
